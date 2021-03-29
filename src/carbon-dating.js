cb6f0a4c546adeb4080ee6c3eda0579879a6b806
VityaRa <blackops2in@mail.ru>
2021-03-29 14:17:36 +0300

    feat: add solution to carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string' && parseFloat(sampleActivity) > 0){
+    let count = 0.693/HALF_LIFE_PERIOD;
+    let ser = Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity));
+    let res = Math.ceil(ser/count);
+        if (res < 0){
+          return false;
+        } else {
+          return res;
+        } 
+  } else {
+    return false;
+  }
 };
